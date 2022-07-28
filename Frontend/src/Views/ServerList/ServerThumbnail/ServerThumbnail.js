363d78c9b445c0360748652ef0b32f56a5a08cae
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2022-07-28 23:08:50 +0200

    Simple server list

--- Frontend/src/Views/ServerList/ServerThumbnail/ServerThumbnail.js
+++ Frontend/src/Views/ServerList/ServerThumbnail/ServerThumbnail.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './ServerThumbnail.css'
 import { ThemeContext } from '../../../App';
+import {Link} from 'react-router-dom';
 
 class ServerThumbnail extends React.Component{
     static contextType = ThemeContext;
@@ -10,9 +11,17 @@ class ServerThumbnail extends React.Component{
     }
     render(){
         return(
-            <div className={this.context === 'dark' ? 'serverCard' : 'serverCard serverCard-light'}>
-                {this.server.name}
+            <Link to={"/" + this.server.serverId}>
+            <div className={this.context === 'dark' ? 'serverCard row' : 'serverCard serverCard-light row'}>
+                <div className='col-1' style={{minWidth:'150px'}}>
+                    <img src={this.server.image} alt={this.server.name} className='serverCard-image'/>
+                </div>
+                <div className='col'>
+                    <div className='serverCard-name'>{this.server.name}</div>
+                    <div className='serverCard-motd'>{this.server.motd}</div>
+                    <div className='serverCard-players'>{this.server.players}/{this.server.maxPlayers}</div></div>
             </div>
+            </Link>
         )
     }
 }
