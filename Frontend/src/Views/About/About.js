530b7214846e5e84779d9414b8313cd06d9626ae
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2022-07-30 15:05:47 +0200

    About section

--- Frontend/src/Views/About/About.js
+++ Frontend/src/Views/About/About.js
@@ -1,11 +1,25 @@
 import './About.css';
 import React from 'react';
+import config from '../../config';
 
 class About extends React.Component {
     render(){
         return(
-            <div className='h3'>
-                About
+            <div>
+                <div className='minecraft title'>
+                    About {config.name}
+                </div>
+                <div className='h2 row'>
+                    <div className='col-10'>         
+                        Cubelist is a place where you can find great Minecraft servers. Ranking is only based on amount of players online.
+                        No buying votes or premium accounts. Everything is free and open source. Have fun!
+                    </div>
+                    <div className='col-2'>
+                        <img src='logo.png' alt={config.name} width="150px" className='img-fluid float-end'/>
+                    </div>
+                    <br />
+                    <a className='githubLink' href='https://github.com/Nasus20202/MCserverList' target={'_blank'} rel={'noreferrer'}>Check my spaghetti code</a>
+                </div>
             </div>
         )
     }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Minecraft Server List
-Created using React JS and ASP.NET Core
+Created using React JS and ASP.NET Core. Easy to use and free Minecraft server list without any redundant stuff.
+
+###[Check it out here (CubeList)!](https://cubelist.knasuta.me/)
+Cubelist is a place where you can find great Minecraft servers. Ranking is only based on amount of players online. 
+No buying votes or premium accounts. Everything is free and open source. Have fun!
